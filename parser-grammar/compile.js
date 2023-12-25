a76d343ec849721c4c870de13511d62192e85674
FrostIce482 <frostice48260@gmail.com>
2023-12-25 23:47:32 +0700

    include peg$SyntaxError in compiling grammar

--- parser-grammar/compile.js
+++ parser-grammar/compile.js
@@ -4,6 +4,10 @@ const fs = require('fs')
 
 const grammar = fs.readFileSync(__dirname + '/parser-grammar.peggy').toString()
 const parser = peggy.generate(grammar)
-const compress = 'module.exports = ' + uglify.minify(parser.parse.toString()).code
+
+const parserStr = parser.SyntaxError.toString()
+    + ';peg$SyntaxError.buildMessage = ' + parser.SyntaxError.buildMessage.toString()
+    + ';module.exports = ' + parser.parse.toString()
+const compress = uglify.minify(parserStr).code
 
 fs.writeFileSync(__dirname + '/../src/parser-compiled.js', compress)
